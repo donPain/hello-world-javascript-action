3fa5b941cc31ea5a18e3932a7cb5e626346b78ea
= <eduardo.plis@hotmail.com>
2021-10-21 19:10:38 -0300

    Testando informações retornadas do payload

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-helloWorld()
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-function helloWorld (){
-  console.log('hello world')
-
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = "maco";//core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+  const ResJson = JSON.parse(payload);
+  console.log(ResJson.commits[0].author);
+} catch (error) {
+  core.setFailed(error.message);
 }
