b55ed99bae3d843e93823cec1a3b5771588f9491
= <eduardo.plis@hotmail.com>
2021-10-21 19:19:30 -0300

    testando

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
   const ResJson = JSON.parse(payload);
-  console.log(ResJson.commits[0].author);
+  console.log(ResJson.commits[0].author.username);
 } catch (error) {
   core.setFailed(error.message);
 }
